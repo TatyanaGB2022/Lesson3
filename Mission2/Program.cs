690c1deaba86c7ca0cfe02a24c8f3a1282d86f33
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 16:07:47 +0600

    Mission2

---
+++ Mission2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+// возможных координат точек в этой четверти (x и y).
+
+int Prompt(string message)
+{
+  System.Console.Write($"{message}: ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+string GetCoord(int quorter)
+{
+  if (quorter == 1)
+  {
+    return "x > 0; y > 0";
+  }
+  if (quorter == 2)
+  {
+    return "x < 0, y > 0";
+  }
+  if (quorter == 3)
+  {
+    return "x < 0, y < 0";
+  }
+  return "x > 0, y < 0";
+}
+
+int a = Prompt("Введите номер четверти");
+if (a > 4 || a < 1)
+{
+  System.Console.WriteLine("Введен не корректный номер четверти");
+}
+else 
+  
+{
+string result = GetCoord(a);
+System.Console.WriteLine(result);
+}
