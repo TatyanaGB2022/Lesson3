5d88b5f5583ce6eb42031c3183106115b2fbbbd3
Tatyana <tatyana.kurii@mail.ru>
2023-01-31 17:11:44 +0600

    Mission3

---
+++ Mission3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+int Prompt(string message)
+{
+  System.Console.Write($"{message}: ");
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = Prompt("Введите координату x первой точки");
+int y1 = Prompt("Введите координату y первой точки");
+int x2 = Prompt("Введите координату x второй точки");
+int y2 = Prompt("Введите координату y второй точки");
+
+double Distance(double x1, double x2, double y1, double y2)
+{
+  double a; double b;
+  a = x2 - x1;
+  b = y2 - y1;
+  return Math.Sqrt(a * a + b * b);
+}
+System.Console.Write(Distance(x1, x2, y1, y2));
